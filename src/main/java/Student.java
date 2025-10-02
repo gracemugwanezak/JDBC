a41e03998de3d082d7195bad929d250361cd504f
Grace MUGWANEZA <gracemugwaneza018@gmail.com>
2025-10-02 16:30:26 +0200

    inserting marks

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -10,9 +10,29 @@ public class Student {
             pstmt.setInt(1,2);
             pstmt.setInt(2,3);
             pstmt.setFloat(3,40);
-
-
             pstmt.executeUpdate();
+
+            pstmt.setInt(1,4);
+            pstmt.setInt(2,3);
+            pstmt.setFloat(3,70);
+            pstmt.executeUpdate();
+
+            pstmt.setInt(1,3);
+            pstmt.setInt(2,5);
+            pstmt.setFloat(3,50);
+            pstmt.executeUpdate();
+
+            pstmt.setInt(1,1);
+            pstmt.setInt(2,2);
+            pstmt.setFloat(3,70);
+            pstmt.executeUpdate();
+
+
+
+
+
+
+
             System.out.println("Marks inserted");
             pstmt.close();
         }
